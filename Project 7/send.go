f47c4b7da2c7dc2fbb1e2152ef8ca45c782603d9
Gromovoi Stanislav <gromila12@rambler.ru>
2023-10-25 16:14:28 +0600

    Project 7

---
+++ Project 7/send.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+	"os"
+
+	"github.com/joho/godotenv"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	// Загрузите переменные среды из файла .env
+	err := godotenv.Load(".env")
+	failOnError(err, "Error loading .env file:")
+
+	url := os.Getenv("CLOUDAMQP_URL")
+	if url == "" {
+		url = "amqp://localhost"
+	}
+
+	conn, err := amqp.Dial(url)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	body := "Hello World!"
+	err = ch.PublishWithContext(ctx,
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s\n", body)
+}
